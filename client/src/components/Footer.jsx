9e3181791434d34c3e70ff8e7b1da46a55852b28
jul1alcaraz <lic.alcarazaballay2022@gmail.com>
2024-04-11 16:26:03 -0300

    mi firma

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import Social from './Social'
-import { Link, useNavigate } from "react-router-dom";
-import { AppBar, Divider, IconButton, Toolbar, Button, Typography, Box, Paper } from "@mui/material";
+import Social from './Social';
+import LocalFloristSharpIcon from '@mui/icons-material/LocalFloristSharp';
+
+import { Paper } from "@mui/material";
+
 
 function Footer(props) {
     return (
-
-
-        <Paper sx={{
-            marginTop: 'calc(10% + 60px)',
-            position: 'fixed',
-            bottom: 0,
-            padding: 2,
-            width: '100%',
-            textAlign: 'center',
-            backgroundColor: 'rgb(227, 9, 25)'
-        }} square variant="outlined" >
-            <Social></Social>
+        <Paper
+            sx={{
+                marginTop: 'calc(10% + 60px)',
+                position: 'fixed',
+                bottom: 0,
+                padding: 2,
+                width: '100%',
+                textAlign: 'center',
+             
+                 // Alinear elementos a la derecha
+                alignItems: 'center', // Alinear verticalmente al centro
+                backgroundColor: 'rgb(227, 9, 25)'
+                
+            }}
+            square
+            variant="outlined"
+        >
+            <Social />
+            <div style={{ color: 'white' }}>By Julieta Alcaraz <LocalFloristSharpIcon></LocalFloristSharpIcon> </div>
         </Paper>
     );
 }
